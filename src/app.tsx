3f3a0d04de33db71a3dec7d0953c414793fddf0d
yinbing.liu <yinbing.liu@upai.com>
2023-05-09 17:34:05 +0800

    feat: 🚀 初始化项目配置

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-
+let a: number;
 const App = () => {
   return (
     <div>
-      <div>
-        配置vite + ts + react
-      </div>
+      <div>配置vite + ts + react</div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
